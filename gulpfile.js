def289c9dc9ad250f933c1405ee008260e2fe039
YllkaBicaj <y.bicaj@gmail.com>
2022-08-13 01:28:41 +0200

    commits

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass') (require('sass'));
+
+function compileSass(done){
+    gulp.src('./scss/main.scss')             
+    .pipe(sass().on('error', sass.logError))     
+    .pipe(gulp.dest('./css'));                 
+    done();
+}
+exports.sass = gulp.parallel(compileSass);
+exports.default = gulp.parallel(compileSass);
